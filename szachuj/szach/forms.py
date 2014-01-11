66f8d3152c28899d88f643c6768e1049d615fee9
Filip Borowiec <filipborowiec@gmail.com>
2014-01-11 16:48:16 +0100

    Adding some bootstrap

--- szachuj/szach/forms.py
+++ szachuj/szach/forms.py
@@ -1,7 +1,14 @@
 from django import forms
+from django.forms import TextInput, Textarea
 from models import Szach
 
 
 class SzachForm(forms.ModelForm):
     class Meta:
         model = Szach
+        widgets = {
+            'signature': TextInput(attrs={'class': 'form-control',
+                                           'placeholder': 'Signature'}),
+            'content': Textarea(attrs={'class': 'form-control',
+                                           'placeholder': 'Content'})
+        }
